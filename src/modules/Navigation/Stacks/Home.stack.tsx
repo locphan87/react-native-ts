98098afa600a548155ffd6c71117ccc4d00c9633
Loc Phan <phanhoangloc@gmail.com>
2018-09-19 00:49:19 +0700

    chore: clean up code, upgrade deps

--- src/modules/Navigation/Stacks/Home.stack.tsx
+++ src/modules/Navigation/Stacks/Home.stack.tsx
@@ -1,10 +1,13 @@
-import { createStackNavigator } from 'react-navigation'
+import { contains } from 'ramda'
+import { StatusBar } from 'react-native'
+import { createStackNavigator, NavigationActions } from 'react-navigation'
 
+import { isAndroid } from '../../../utils/platform.util'
 import Routes from '../Navigation.constant'
 import { stackOptions } from '../Navigation.option'
 import Screens from '../Navigation.screen'
 
-export default createStackNavigator(
+const HomeStack = createStackNavigator(
   {
     [Routes.Home.Home]: Screens.Home
   },
@@ -13,3 +16,36 @@ export default createStackNavigator(
     navigationOptions: stackOptions
   }
 )
+const defaultGetStateForAction = HomeStack.router.getStateForAction
+const LightStatusBarRoutes = []
+const NoStatusBarRoutes = [Routes.Home.Home]
+const { BACK, NAVIGATE, INIT } = NavigationActions
+const allowedActions = [INIT, NAVIGATE, BACK]
+
+HomeStack.router.getStateForAction = (action, state) => {
+  const { routeName, type } = action
+  const newState = defaultGetStateForAction(action, state)
+
+  if (!contains(type, allowedActions)) {
+    return newState
+  }
+
+  StatusBar.setBarStyle('dark-content')
+  StatusBar.setHidden(false)
+
+  if (isAndroid()) {
+    StatusBar.setTranslucent(true)
+    StatusBar.setBackgroundColor('transparent')
+  }
+
+  if (contains(routeName, LightStatusBarRoutes)) {
+    StatusBar.setBarStyle('light-content')
+  }
+  if (contains(routeName, NoStatusBarRoutes) || type === INIT) {
+    StatusBar.setHidden(true)
+  }
+
+  return newState
+}
+
+export default HomeStack
